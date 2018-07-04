53595120d90f2736e6dfe12d535c3f2f4c444eb8
Poul Kjeldager Sørensen <pks@s-innovations.net>
2018-07-04 09:00:49 +0200

    Add project files.

---
+++ DotNetDevOps.Vsts.CSharp.Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetDevOps.Vsts.CSharp.Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
